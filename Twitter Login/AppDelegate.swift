c0be819c5675b83fa8a82ef29b7c6a38afe68ee6
kumar laxman jee <kralxman.mec@gmail.com>
2015-10-21 12:40:59 +0530

    Completed twitter login

--- Twitter Login/AppDelegate.swift
+++ Twitter Login/AppDelegate.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import Fabric
+import TwitterKit
+
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
 
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+    {
         // Override point for customization after application launch.
+        
+        Fabric.with([Twitter()])
+
+        
         return true
     }
 
--- Twitter Login/ViewController.swift
+++ Twitter Login/ViewController.swift
@@ -7,15 +7,29 @@
 //
 
 import UIKit
+import TwitterKit
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let logInButton = TWTRLogInButton(logInCompletion: {
+            (session: TWTRSession!, error: NSError!) in
+            
+            print(session.userName)
+            
+        })
+        
+        logInButton.center = self.view.center
+        self.view.addSubview(logInButton)
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
